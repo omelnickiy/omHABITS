14c3897a6fcacc8c14b331a9171ff88bde29ea35
omelnickiy <93851921+omelnickiy@users.noreply.github.com>
2025-09-30 20:47:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import List, Dict, Tuple, Set
 import calendar
